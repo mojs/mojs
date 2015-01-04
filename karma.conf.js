ca1a84b7a09e714b120dd1f2232263ccdffd70c4
legomushroom <legomushroom@gmail.com>
2015-01-04 11:47:15 +0200

    add info to package.json

--- karma.conf.js
+++ karma.conf.js
@@ -81,7 +81,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS', 'Chrome'],
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
