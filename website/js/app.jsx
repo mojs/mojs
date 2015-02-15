4769eeb6a967dde09a3c62fd1550233da5757d97
Oleg Solomka <legomushroom@gmail.com>
2015-02-15 15:30:45 +0200

    add website router

--- website/js/app.jsx
+++ website/js/app.jsx
@@ -1,11 +1,64 @@
 import React from 'react';
+
+var Router = require('react-router')
+var DefaultRoute = Router.DefaultRoute;
+var Link = Router.Link;
+var Route = Router.Route;
+var RouteHandler = Router.RouteHandler;
+
+
 // require('../css/main.styl');
 React.initializeTouchEvents(true);
 
 let App = React.createClass({
   render(){
-    return <div></div>
+    return (
+      <div>
+        <header>
+          <ul>
+            <li><Link to="app">Dashboard</Link></li>
+            <li><Link to="inbox">Inbox</Link></li>
+            <li><Link to="calendar">Calendar</Link></li>
+          </ul>
+          Logged in as Jane
+        </header>
+
+        {/* this is the important part */}
+        <RouteHandler/>
+      </div>
+    );
   }
 });
 
-React.render(<App />, document.body);
+let Inbox = React.createClass({
+  render: function() {
+    return <div>Inbox</div>
+  }
+});
+
+let Calendar = React.createClass({
+  render: function() {
+    return <div>Calendar</div>
+  }
+});
+
+let Dashboard = React.createClass({
+  render: function() {
+    return <div>Dashboard</div>
+  }
+});
+
+var routes = (
+  <Route name="app" path="/" handler={App}>
+    <Route name="inbox" handler={Inbox}/>
+    <Route name="calendar" handler={Calendar}/>
+    <Route name="dashboard" path="/dashboard" handler={Dashboard}></Route>
+    <DefaultRoute handler={Dashboard}/>
+  </Route>
+);
+
+Router.run(routes, Router.HistoryLocation, function (Handler) {
+  React.render(<Handler/>, document.body);
+});
+
+// React.render(<App />, document.body);
