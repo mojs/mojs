6c64fe13caf345ab60b9aae18077955ce6b21e0f
Oleg Solomka <legomushroom@gmail.com>
2015-11-24 13:58:46 +0200

    move tweener module to es6

---
+++ js/tween/tweener.babel.js
@@ -0,0 +1,52 @@
+require('../polyfills/raf');
+require('../polyfills/performance');
+import h from '../h';
+
+class Tweener {
+  constructor() { this.vars(); return this; }
+  
+  vars () {
+    this.tweens = []; this.loop = h.bind(this.loop, this);
+  }
+
+  loop() {
+    if (!this.isRunning) { return false; }
+    var time = performance.now(); this.update(time);
+    if (!this.tweens.length) { return this.isRunning = false; }
+    requestAnimationFrame(this.loop);
+    return this;
+  }
+
+  startLoop() {
+    if (this.isRunning) { return; }; this.isRunning = true
+    requestAnimationFrame(this.loop);
+  }
+
+  stopLoop() { this.isRunning = false; }
+
+  update(time) {
+    var i = this.tweens.length;
+    while(i--) {
+      if (this.tweens[i].update(time) === true) { this.remove(i); }
+    }
+  }
+
+  add(tween) {
+    this.tweens.push(tween); this.startLoop();
+  }
+  
+  removeAll() {
+    this.tweens.length = 0;
+  }
+
+  remove(tween) {
+    var index = (typeof tween === 'number')
+      ? tween
+      : this.tweens.indexOf(tween);
+
+    if (index !== -1) { this.tweens.splice(index, 1); }
+  }
+}
+  
+var t = new Tweener
+export default t;
--- karma.conf.js
+++ karma.conf.js
@@ -117,7 +117,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-        'dist/mo.js': 'coverage',
+        'build/mo.js': 'coverage',
         // 'dist/**/*.js': ['browserify']
     },
     // browserify: {
