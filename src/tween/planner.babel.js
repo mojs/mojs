96a46f0bfe518b9c914c8105bf8094b5deb9a5e6
Oleg Solomka <legomushroom@gmail.com>
2017-05-14 22:43:02 -0500

    [planner]: initial commit

--- src/tween/planner.babel.js
+++ src/tween/planner.babel.js
@@ -23,60 +23,24 @@ Planner._vars = function() {
 Planner.createPlan = function() {
   // reset plan
   this._plan.length = 0;
-  // recalculate total duration time
-  this._calcTotalTime();
-
   // frame size (60fps)
   const step = 16;
 
-  const { delay, duration, repeat } = this._props;
+  const { duration } = this._props;
   // current time shift by `delay`
   // then add `8` to be sure we always are in the middle of the frame
-  let time = delay + (step/2);
+  let time = (step/2);
 
-  while (time <= this._totalTime) {
-    const prevPeriod = this._getPeriod(time - step);
-    const period = this._getPeriod(time);
-    const nextPeriod = this._getPeriod(time + step);
-    const prevFrame = this._plan[this._plan.length-1];
+  // *  0 -> onUpdate
+  // *  1 -> onStart&onChimeIn
+  // *  2 -> onEnd&onChimeOut
 
-    // delay
+  while (time <= duration) {
     let frameSnapshot = 0;
-    if (period !== 'delay') {
-      frameSnapshot = 1;
-    }
-
-    // *  0 -> none
-    // *  1 -> onUpdate
-    // *  2 -> onStart&onChimeIn
-    // *  3 -> onEnd&onChimeOut
-
-    const isPrevFrame = prevFrame !== undefined;
-    const isPrevDelay = prevPeriod === 'delay';
 
     // onStart
-    if (!isPrevFrame) {
-      frameSnapshot = frameSnapshot | (1 << 4);
-    }
-
-    // onRepeatStart
-    if (!isPrevFrame || (isPrevDelay && period !== 'delay') || prevPeriod === period - 1) {
-      frameSnapshot = frameSnapshot | (1 << 6);
-    }
-
-    // onRepeatStartBackward
-    if ((period === 'delay' && nextPeriod !== 'delay') || (nextPeriod === period + 1 && nextPeriod < this._props.repeat+1) ) {
-      frameSnapshot = frameSnapshot | (1 << 7);
-    }
-
-    // onRepeatComplete
-    if ((period === 'delay' && prevPeriod !== 'delay') || (prevPeriod === period - 1 && prevPeriod !== -1)) {
-      frameSnapshot = frameSnapshot | (1 << 8);
-    }
-
-    // onRepeatCompleteBackward
-    if (period !== 'delay' && nextPeriod === 'delay' || nextPeriod === period + 1) {
-      frameSnapshot = frameSnapshot | (1 << 9);
+    if (time === step/2) {
+      frameSnapshot = frameSnapshot | (1 << 1);
     }
 
     this._plan.push(frameSnapshot);
@@ -85,7 +49,7 @@ Planner.createPlan = function() {
 
   // onCompleteBackward
   const lastIndex = this._plan.length - 1;
-  this._plan[lastIndex] = this._plan[lastIndex] | (1 << 11);
+  this._plan[lastIndex] = this._plan[lastIndex] | (1 << 2);
 
   return this._plan;
 }
