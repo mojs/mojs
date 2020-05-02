7c9927ec8a6b340fd91636a8e21bc1dd865ce860
Xavier Foucrier <xavier.foucrier@gmail.com>
2020-05-02 18:39:53 +0200

    Simplify webpack build

--- webpack.umd.js
+++ webpack.umd.js
@@ -12,12 +12,10 @@ module.exports = (argv) => merge(require('./webpack.common.js')(argv), {
   watch: false,
   output: {
     filename: 'mo.umd.js',
-    publicPath: 'dist/',
     library: 'mojs',
     libraryExport: 'default',
     libraryTarget: 'umd',
-    umdNamedDefine: true,
-    globalObject: 'this'
+    umdNamedDefine: true
   },
   optimization: {
     minimizer: [
