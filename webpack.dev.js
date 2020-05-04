b638e31a656b2be220ddaf4b200bb11a37d13060
Xavier Foucrier <xavier.foucrier@gmail.com>
2020-05-04 12:02:19 +0200

    Move the --watch option in order to build before running tests

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,7 +5,6 @@ const merge = require('webpack-merge');
 
 module.exports = (argv) => merge(require('./webpack.common.js')(argv), {
   mode: 'development',
-  watch: true,
   output: {
     filename: 'mo.js'
   },
