46448a3e02ac4d9bd2164211fe076a98f474a3f1
Kostas Minaidis <minaidisk@gmail.com>
2023-08-28 02:54:30 +0300

    Finished unshift demo

--- dev/index.js
+++ dev/index.js
@@ -1,10 +1,13 @@
 import mojs from 'src/mojs.babel.js';
 import MojsPlayer from '@mojs/player';
 
-const orange  = "#f59d56";
-const green   = "#aded94";
-const magenta = "#bd81fb";
-const red     = "#ee695a"
+const ORANGE  = "#f59d56";
+const GREEN   = "#aded94";
+const MAGENTA = "#bd81fb";
+const RED     = "#ee695a"
+
+const downArrow = "&#8681;";
+const upArrow   = "&#8679;";
 
 const xDiff              = 536 - 22;
 const leftBracketX       = 22;
@@ -18,72 +21,16 @@ const bracketRightMargin = 22;
 const rightArrowX        = 460;
 const rightArrowY        = 76;
 
-const marbleOptions = {
-  className: "mojs-marble",
-  radius: marbleRadius,
-  isShowStart: true,
-  left: leftMarbleX,
-  top: 100,
-  y: 0
+function uuid() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
 }
 
-function getBracket({ id, dir = "left", x = 0, y = 0, ...args }, parent ){
+function initializeMethod({ el, jsCode, method, active = false, indexed = true }){
 
-  const bracketEl = addBracketElToParent({ dir, ...args }, parent );
-
-  return new mojs.Html({ el: bracketEl, x, y })
-
-}
-
-function addBracketElToParent({ id, dir = "left" }, parent ){
-
-  const bracketEl = document.createElement("div");
-  bracketEl.setAttribute("class", "char");
-  if ( id ) {
-    bracketEl.setAttribute("id", id);
-  }
-  bracketEl.textContent = dir === "left" ? "[" : "]";
-  parent.appendChild(bracketEl);
-  return bracketEl;
-
-}
-
-function getMarble({ fill = orange, x = 0, y = 0, opacity = 0, index = null, ...args } = {}){
-
-  return new mojs.Shape({
-    ...marbleOptions,
-    x,
-    opacity: 1,
-    fill,
-    attrs: typeof index === "number" ? [{ "data-index": index }] : null,
-    ...args
-  })
-
-}
-
-function getMarblesFromInitialX({ count = 0, x = 0, fill, index = false, xOffset = 0, ...args }){
-
-  if ( count === 0 ){
-    return [];
-  }
-
-  const marbles = [];
-
-  for (let i = 0; i < count; i++) {
-
-    marbles.push(getMarble({
-      x: (i * marbleWidth) + xOffset,
-      fill,
-      index: i
-    }));
-
-  }
-
-  return marbles;
-
-}
-
-function initializeMethod(el){
+  const methodId = uuid();
 
   const html = `
     <main>
@@ -91,93 +38,209 @@ function initializeMethod(el){
         <button class="demo-init">&#9658; Play</button>
         <button class="demo-reset" hidden>Reset</button>
       </div>
-      <div class="code-show">
+      <div class="code-show" id="code-show-${methodId}">
         <button>
-          <span class="arrow">&#8679;</span>
-          <span class="text">Hide Code</span>
+          <span class="arrow">${active ? upArrow : downArrow}</span>
+          <span class="text">${active ? "Hide Code" : "Show Code"}</span>
         </button>
       </div>
     </main>
-    <pre class="code active"></pre>
+    <pre class="code ${active ? "active" : ""}"></pre>
   `
 
   el.setAttribute("class", "demo-container");
   el.insertAdjacentHTML(
     "afterbegin",
     html
-  )
+  );
+
+  const parent = el.querySelector("main");
+
+  const marbleOptions = {
+    parent,
+    className: indexed ? "mojs-marble indexed" : "mojs-marble",
+    radius: marbleRadius,
+    isShowStart: true,
+    left: leftMarbleX,
+    top: 100,
+    y: 0
+  }
+
+  const rightArrowHTML = `<div class="char" id="long-right-arrow${methodId}">&xrarr;</div>`
+
+  parent.insertAdjacentHTML("afterbegin", rightArrowHTML)
+
+  parent.insertAdjacentHTML("afterbegin", method);
+
+  el.querySelector("pre").insertAdjacentHTML(
+    "beforeend",
+    `<code class="language-javascript">${jsCode}</code>`
+  );
+
+  el.querySelector(`#code-show-${methodId}`).addEventListener("click", ()=>{
+
+    const preEl = el.querySelector("pre.code");
+    const arrow = el.querySelector("span.arrow");
+    const arrowText = el.querySelector("button span.text");
+    if ( preEl.classList.contains("active")){
+      arrow.innerHTML = downArrow;
+      arrowText.textContent = "Show Code";
+      return preEl.classList.remove("active");
+    }
+    arrowText.textContent = "Hide Code";
+    arrow.innerHTML = upArrow;
+    return preEl.classList.add("active");
+  });
+
+  const rightArrow = new mojs.Html({
+    el: `#long-right-arrow${methodId}`,
+    scale: 2,
+    x: rightArrowX,
+    y: rightArrowY,
+    duration: 3000,
+  })
+
+  function getBracket({ id, dir = "left", x = 0, y = 0, ...args } ){
+
+    const bracketEl = addBracketElToParent({ dir }, parent );
+
+    return new mojs.Html({ el: bracketEl, x, y, ...args })
+
+  }
+
+  function addBracketElToParent({ id, dir = "left" } ){
+
+    const bracketEl = document.createElement("div");
+    bracketEl.setAttribute("class", "char");
+    if ( id ) {
+      bracketEl.setAttribute("id", id);
+    }
+    bracketEl.textContent = dir === "left" ? "[" : "]";
+    parent.appendChild(bracketEl);
+    return bracketEl;
+
+  }
+
+  function getMarble({ fill = ORANGE, x = 0, y = 0, opacity = 0, index = null, ...args } = {}){
+
+    return new mojs.Shape({
+      ...marbleOptions,
+      x,
+      opacity: 1,
+      fill,
+      attrs: typeof index === "number" ? [{ "data-index": index }] : null,
+      ...args
+    })
+
+  }
+
+  function getMarblesFromInitialX({ count = 0, x = 0, fill, index = false, xOffset = 0, ...args }){
+
+    if ( count === 0 ){
+      return [];
+    }
+
+    const marbles = [];
+
+    for (let i = 0; i < count; i++) {
+
+      marbles.push(getMarble({
+        x: (i * marbleWidth) + xOffset,
+        fill,
+        index: i,
+        ...args
+      }));
+
+    }
+
+    return marbles;
+
+  }
+
+  let hasInitialized = false;
+
+  function init(fn){
+    if ( !hasInitialized ){
+      hasInitialized = true;
+      el
+      .querySelector(".demo-init")
+      .addEventListener("click",  fn, { once: true });
+    }
+  }
+
+  return {
+    getMarble,
+    getMarblesFromInitialX,
+    getBracket,
+    init
+  }
 
 }
 
 //>> EXAMPLE #1: push()
 array_push: {
 
+  // MODIFY:
+  const pushJS = `
+    const letters = [ "A", "B", "C" ];
+    letters.push( "D" );
+
+    console.log( letters ); // [ "A", "B", "C", "D" ];
+  `
+
+  // MODIFY:
+  const method = `
+  <div class="char method" id="push">.push(<span class="c-space"></span>)</div>
+  `
+  // MODIFY:
   const demoContainer      = document.querySelector("section#array-push");
-  initializeMethod(demoContainer);
 
-  const parent             = demoContainer.querySelector("main");
-  marbleOptions.parent     = parent;
-
-  const rightArrowHTML = `<div class="char" id="long-right-arrow">&xrarr;</div>`
-  parent.insertAdjacentHTML("afterbegin", rightArrowHTML)
-
-  parent.insertAdjacentHTML("afterbegin", `
-    <div class="char method" id="push">
-      .push(<span class="c-space"></span>)
-    </div>
-  `);
+  const { init, getBracket, getMarble, getMarblesFromInitialX } = initializeMethod({
+    el: demoContainer,
+    jsCode: pushJS,
+    method,
+    active: true
+  });
 
   const arrayMethodsVisualizedTimeline = new mojs.Timeline({ repeat: 1 });
-
   const circlesInput = getMarblesFromInitialX({ count: 4, index: true });
   const circlesOutput = getMarblesFromInitialX({ count: 4, index: true, xOffset: xDiff });
 
-  const pushBallInitial = new mojs.Shape({
-    ...marbleOptions,
+  // MODIFY:
+  const pushBallInitial = getMarble({
     x: 254,
     duration: 1000,
-    fill: green,
-    onComplete: () => {
-      console.log("onComplete()", pushBallInitial, pushBallInitial.x)
-    }
+    fill: GREEN
   });
 
+  // MODIFY:
   const pushBall = getMarble({
     x: { 254: 666 },
     duration: 3000,
     opacity: { 0: 1 },
-    fill: green,
+    fill: GREEN,
     onComplete: () => {
       pushBall.getProps().el.setAttribute("data-index", 4);
     }
 
   });
 
-  const pushJS = `
-    const letters = [ "A", "B", "C", "D" ];
-    letters.push( "E" );
-
-    console.log( letters ); // [ "A", "B", "C", "D", "E" ];
-  `
-  demoContainer.querySelector("pre").insertAdjacentHTML(
-    "beforeend",
-    `<code class="language-javascript">${pushJS}</code>`
-  );
-
   const leftBracket = getBracket({
     x: leftBracketX,
     y: leftBracketY
-  }, parent );
+  });
 
   const rightBracket = getBracket({
     x: leftBracketX + ( 4 * marbleWidth ) + bracketPadding,
-    y: leftBracketY, duration: 3000, dir: "right"
-  }, parent );
+    y: leftBracketY,
+    duration: 3000,
+    dir: "right"
+  });
 
   const leftBracket2 = getBracket({
     x: leftBracket2X,
     y: leftBracketY
-  }, parent );
+  });
 
   const initialRightBracket2X = leftBracket2X + ( 4 * marbleWidth ) + bracketPadding;
 
@@ -189,16 +252,9 @@ array_push: {
     },
     y: leftBracketY,
     duration: 3000,
-  }, parent );
+  });
 
-  const rightArrow = new mojs.Html({
-    el: "#long-right-arrow",
-    scale: 2,
-    x: rightArrowX,
-    y: rightArrowY,
-    duration: 3000,
-  })
-
+  // MODIFY:
   const push = new mojs.Html({
     el: "#push",
     x: leftBracketX + ( 4 * marbleWidth ) + bracketPadding + bracketRightMargin,
@@ -209,30 +265,143 @@ array_push: {
   arrayMethodsVisualizedTimeline
   .add(circlesInput, circlesOutput)
 
-  document.querySelector(".demo-init").addEventListener("click", () => {
+  init(()=>{
     pushBall.play()
     rightBracket2.play();
     pushBallInitial.play();
   });
 
-  document.querySelector(".code-show").addEventListener("click", ()=>{
-    const downArrow = "&#8681;";
-    const upArrow = "&#8679;";
-    const preEl = document.querySelector("pre.code");
-    const arrow = document.querySelector("span.arrow");
-    const arrowText = document.querySelector("button span.text");
-    if ( preEl.classList.contains("active")){
-      arrow.innerHTML = downArrow;
-      arrowText.textContent = "Show Code";
-      return preEl.classList.remove("active");
+}
+
+//>> EXAMPLE #2: unshift()
+array_unshift : {
+
+  // MODIFY:
+  const pushJS = `
+    const letters = [ "B", "C", "D" ];
+    letters.unshift( "A" );
+
+    console.log( letters ); // [ "A", "B", "C", "D" ];
+  `
+  // MODIFY:
+  const method = `
+  <div class="char method" id="unshift">.unshift(<span class="c-space"></span>)</div>
+  `
+  // MODIFY:
+  const demoContainer = document.querySelector("section#array-unshift");
+
+  const { init, getBracket, getMarble, getMarblesFromInitialX } = initializeMethod({
+    el: demoContainer,
+    jsCode: pushJS,
+    method,
+    indexed: false
+  });
+
+  const arrayMethodsVisualizedTimeline = new mojs.Timeline({ repeat: 1 });
+  const circlesInput = getMarblesFromInitialX({ count: 4, index: true });
+  const circlesOutput = getMarblesFromInitialX({
+    count: 4,
+    index: true,
+    xOffset: xDiff,
+    duration: 3000
+  });
+
+  // MODIFY:
+  const initialX = 283;
+  const pushBallInitial = getMarble({
+    x: initialX,
+    duration: 1000,
+    fill: GREEN
+  });
+
+  // MODIFY:
+  const pushBall = getMarble({
+    x: { [initialX] : xDiff },
+    duration: 3000,
+    opacity: { 0: 1 },
+    fill: GREEN,
+    onComplete: () => {
+      pushBall.getProps().el.setAttribute("data-index", 0);
     }
-    arrowText.textContent = "Hide Code";
-    arrow.innerHTML = upArrow;
-    return preEl.classList.add("active");
+
+  })
+
+  const leftBracket = getBracket({
+    x: leftBracketX,
+    y: leftBracketY
+  });
+
+  const rightBracket = getBracket({
+    x: leftBracketX + ( 4 * marbleWidth ) + bracketPadding,
+    y: leftBracketY,
+    duration: 3000,
+    dir: "right"
+  });
+
+  const leftBracket2 = getBracket({
+    x: leftBracket2X,
+    y: leftBracketY
+  });
+
+  const initialRightBracket2X = leftBracket2X + ( 4 * marbleWidth ) + bracketPadding;
+
+  const rightBracket2 = getBracket({
+    dir: "right",
+    x: {
+      [initialRightBracket2X]:
+      initialRightBracket2X + marbleWidth
+    },
+    y: leftBracketY,
+    duration: 3000,
+  });
+
+  // MODIFY:
+  const unshift = new mojs.Html({
+    el: "#unshift",
+    x: leftBracketX + ( 4 * marbleWidth ) + bracketPadding + bracketRightMargin,
+    y: leftBracketY,
+    duration: 3000,
+  })
+
+  arrayMethodsVisualizedTimeline
+  .add(circlesInput, circlesOutput)
+
+  // MODIFY:
+  init(()=>{
+    pushBall.play()
+    rightBracket2.play();
+    pushBallInitial.play();
+
+    circlesOutput.forEach((c, i) =>{
+
+      const x = parseInt(c.getProps().x.split("px")[0])
+
+      c.tune({
+        x: { [x] : x + marbleWidth },
+        onComplete: ()=>{
+        c.getProps().el.setAttribute("data-index", i+1);
+      }});
+      c.play();
+
+    })
+
+
   });
 
 }
 
-array_reverse : {
+//>> EXAMPLE #3: pop()
 
-}
+//>> EXAMPLE #4: shift()
+
+//>> EXAMPLE #5: filter()
+
+//>> EXAMPLE #6: map()
+
+//>> EXAMPLE #7: join("-")
+
+//>> EXAMPLE #8: concat()
+
+//>> EXAMPLE #9: flat()
+
+//>> EXAMPLE #10: slice(1,3)
