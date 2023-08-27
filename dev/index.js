5c2810fe31f4c3985aeac69a350e438bc855ec72
Kostas Minaidis <minaidisk@gmail.com>
2023-08-28 00:21:58 +0300

    Updated demo

--- dev/index.js
+++ dev/index.js
@@ -1,97 +1,98 @@
 import mojs from 'src/mojs.babel.js';
 import MojsPlayer from '@mojs/player';
-// mojs.revision;
 
-
-// 2) Examples:
-const burst = new mojs.Burst({
-  shape: 'circle',
-  radius: { 0: 360 },
-  duration: 2000,
-  children: {
-    repeat: 999,
-    radius: { 30: 5 },
-    // 2.1)
-    shape: 'cross',
-    stroke: 'teal',
-    strokeWidth: { 1: 0 },
-    angle: { 360: 0 },
-    duration: 2000
-  },
-  // 2.2)
-  count: 20,
-})// .play();
-
-// 3)
-const circle = new mojs.Shape({
-  radius: { 0: 360 },
-  fill: 'none',
-  stroke: 'yellow'
-})
-const timeline = new mojs.Timeline({
-  repeat: 999
-})
-  .add(burst, circle)
-// .play();
-
-// 4) Implementing our animations...
-
-// 4.1) Inspecting colors from image:
-const orange = "#f59d56";
-const green = "#aded94";
+const orange  = "#f59d56";
+const green   = "#aded94";
 const magenta = "#bd81fb";
-const red = "#ee695a"
+const red     = "#ee695a"
 
-const circlesInput = [];
-const circlesOutput = [];
+const parent             = document.getElementById("app");
+const xDiff              = 536 - 22;
+const leftBracketX       = 22;
+const leftBracketY       = 84;
+const leftMarbleX        = 64;
+const marbleRadius       = 16;
+const marbleWidth        = 38;
+const bracketPadding     = 14;
+const bracketRightMargin = 22;
 
+const circleOptions = {
+  parent,
+  className: "mojs-marble",
+  radius: marbleRadius,
+  isShowStart: true,
+  left: leftMarbleX,
+  top: 100,
+  y: 0
+}
+
+function addBracketEl({ id, dir = "left" }){
+
+  const bracketEl = document.createElement("div");
+  bracketEl.setAttribute("class", "char");
+  if ( id ) {
+    bracketEl.setAttribute("id", id);
+  }
+  bracketEl.textContent = dir === "left" ? "[" : "]";
+  parent.appendChild(bracketEl);
+  return bracketEl;
+
+}
+
+// CREATING HTML ELEMENTS:
+const leftBracketEl = addBracketEl({ id: "left-bracket" });
+const rightBracketEl = addBracketEl({ id: "right-bracket", dir: "right" });
+const leftBracketEl2 = addBracketEl({ id: "left-bracket2" });
+const rightBracketEl2 = addBracketEl({ id: "right-bracket2", dir: "right" });
+
+const rightArrowHTML = `<div class="char" id="long-right-arrow">&xrarr;</div>`
+parent.insertAdjacentHTML("afterbegin", rightArrowHTML)
+
+parent.insertAdjacentHTML("afterbegin", `
+  <div class="char method" id="push">
+    .push(<span class="c-space"></span>)
+  </div>
+`);
+
+// INITIALIZING MOJS OBJECTS:
 const arrayMethodsVisualizedTimeline = new mojs.Timeline({
   repeat: 1
 })
-const parent = document.getElementById("app");
-const xDiff = 536 - 22;
 
-const circleOptions = {
-  parent, // RTFM!!!
-  className: "mojs-marble",
-  radius: 16,
-  isShowStart: true,
-  left: 64,
-  top: 100,
-  y: 0
+const circlesInput  = [];
+const circlesOutput = [];
+
+function getMarble({ fill = orange, x = 0, y = 0, opacity = 0, index = null } = {}){
+
+  return new mojs.Shape({
+    ...circleOptions,
+    x,
+    opacity: 1,
+    fill,
+    attrs: typeof index === "number" ? [{ "data-index": index }] : null,
+  })
+
 }
 
 // PREPARE INPUT BALLS:
 for (let i = 0; i < 4; i++) {
 
-  circlesInput.push(new mojs.Shape({
-    ...circleOptions,
-    x: i === 3 ? { [i * 38]: 300 } : i * 38,
-    opacity: 1,
+  circlesInput.push(getMarble({
+    x: i === 3 ? { [i * marbleWidth]: 300 } : i * marbleWidth,
     fill: i === 4 ? green : orange,
-    attrs: [{
-      "data-index": i
-    }],
-  }))
+    index: i
+  }));
 
 }
 
 // PREPARE OUTPUT BALLS:
-for (let i = 0; i < 5; i++) {
+for (let i = 0; i < 4; i++) {
 
-  if (i === 4) {
-    continue;
-  }
-
-  circlesOutput.push(new mojs.Shape({
-    ...circleOptions,
-    x: (i * 38) + xDiff,
-    opacity: 1,
+  circlesOutput.push(getMarble({
+    x: (i * marbleWidth) + xDiff,
     fill: orange,
-    attrs: [{
-      "data-index": i
-    }]
-  }))
+    index: i
+  }));
 
 }
 
@@ -116,55 +117,57 @@ const pushBall = new mojs.Shape({
   }
 })
 
-const space4 = 0;
-const space5 = 0;
 const leftBracket = new mojs.Html({
-  el: "#left-bracket",
-  x: 22,
-  y: 84
+  el: leftBracketEl,
+  x: leftBracketX,
+  y: leftBracketY
 })
+console.log(leftBracketEl)
 const rightBracket = new mojs.Html({
-  el: "#right-bracket",
-  // x: { 478: 512 }, // 512
-  x: 188,
-  y: 84,
+  el: rightBracketEl,
+  x: leftBracketX + ( 4 * marbleWidth ) + bracketPadding,
+  y: leftBracketY,
   duration: 3000,
 })
 const leftBracket2 = new mojs.Html({
-  el: "#left-bracket2",
+  el: leftBracketEl2,
   x: 536,
   y: 84
 })
 const rightBracket2 = new mojs.Html({
-  el: "#right-bracket2",
+  el: rightBracketEl2,
   // x: 740,
   x: 702,
   x: { 702: 740 },
   y: 84,
   duration: 3000,
 })
+
 const rightArrow = new mojs.Html({
   el: "#long-right-arrow",
+  // el: rightArrowEl,
   scale: 2,
-  x: { 460: 0 },
+  x: 460,
   y: 76,
   duration: 3000,
 })
+
 const push = new mojs.Html({
   el: "#push",
-  x: 210,
-  y: 84,
+  x: leftBracketX + ( 4 * marbleWidth ) + bracketPadding + bracketRightMargin,
+  y: leftBracketY,
   duration: 3000,
 })
 
 arrayMethodsVisualizedTimeline
-  .add(circlesInput, circlesOutput)
+.add(circlesInput, circlesOutput)
 
 document.querySelector(".demo-init").addEventListener("click", () => {
   pushBall.play()
   rightBracket2.play();
   pushBallInitial.play();
 });
+
 document.querySelector(".code-show").addEventListener("click", ()=>{
   const downArrow = "&#8681;";
   const upArrow = "&#8679;";
